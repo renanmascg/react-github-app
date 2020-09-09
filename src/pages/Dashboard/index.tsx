a2decc9e55361b3fc80302a787c7ef98c5d65687
renan mascarenhas goncalves <renanmascg@gmail.com>
2020-09-08 22:08:59 -0300

    Termina a estilização da tela de dashboard dentro da aplicação.

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
-import { Title, Form } from './styles';
+import { FiChevronRight } from 'react-icons/fi';
+import { Title, Form, Repositories } from './styles';
 import logoImg from '../../assets/logo.svg';
 
 const Dashboard: React.FC = () => {
@@ -11,6 +12,20 @@ const Dashboard: React.FC = () => {
         <input type="text" placeholder="Digite o nome do repositório" />
         <button type="submit">Pesquisar</button>
       </Form>
+
+      <Repositories>
+        <a href="teste">
+          <img
+            src="https://avatars3.githubusercontent.com/u/37426438?s=460&u=82aa4d56cd8c6b1072b566906eb090052244b855&v=4"
+            alt="renan mascarenhas"
+          />
+          <div>
+            <strong>rocketseat/unform</strong>
+            <p>Easy peasy package to make someone laught</p>
+          </div>
+          <FiChevronRight size={20} />
+        </a>
+      </Repositories>
     </>
   );
 };
